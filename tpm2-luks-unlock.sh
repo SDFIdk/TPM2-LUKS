f2b913973f5dd8409face09f4c3f7076f179fc5f
Mark Petersen <mapet@sdfi.dk>
2023-06-12 10:54:45 +0200

    fix echo

--- tpm2-luks-unlock.sh
+++ tpm2-luks-unlock.sh
@@ -64,7 +64,7 @@ CheckDependencies () {
 	if ! command -v cryptsetup &> /dev/null
 	then
 		echo "cryptsetup-bin could not be found"
-		echo "please install it with"
+		echo "Please install it with"
 		echo "apt-get install cryptsetup-bin"
 		exit 1
 	fi
@@ -72,7 +72,7 @@ CheckDependencies () {
 	if ! command -v mkinitramfs &> /dev/null
 	then
 		echo "mkinitramfs could not be found"
-		echo "please install it with"
+		echo "Please install it with"
 		echo "apt-get install initramfs-tools-core"
 		exit 1
 	fi
@@ -80,7 +80,7 @@ CheckDependencies () {
 	if ! command -v tpm2_nvread &> /dev/null
 	then
 		echo "tpm2_nvread could not be found"
-		echo "please install it with"
+		echo "Please install it with"
 		echo "apt-get install tpm2-tools"
 		exit 1
 	fi
@@ -88,7 +88,7 @@ CheckDependencies () {
 	if ! command -v sed &> /dev/null
 	then
 		echo "sed could not be found"
-		echo "please install it with"
+		echo "Please install it with"
 		echo "apt-get install sed"
 		exit 1
 	fi
@@ -99,49 +99,49 @@ KeyFileGenerate () {
 	then
 		echo "Key file $KEYFILE already exists"
 	else
-		echo Generating a $KEYSIZE char alphanumeric key and saving it to $KEYFILE...
+		echo "Generating a $KEYSIZE char alphanumeric key and saving it to $KEYFILE..."
 		echo
 		cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c $KEYSIZE > $KEYFILE
 	fi
 }
 
 KeyFileRemove () {
-	echo Removing $KEYFILE file for extra security...
+	echo "Removing $KEYFILE file for extra security..."
 	echo
 	shred -n 10 -u $KEYFILE
 }
 
 TPM2LuksCheck () {
-	echo Check where the TPM2 key already exist in LUKS
+	echo "Check where the TPM2 key already exist in LUKS"
 	echo
 	tpm2_nvread -s $KEYSIZE $KEYADDRESS 2> /dev/null |cryptsetup open --test-passphrase $TARGET_DEVICE 2> /dev/null
 	if [ $? = 0 ]
 	then
-		echo The TPM2 key is already defined in LUKS no changes needed
+		echo "The TPM2 key is already defined in LUKS no changes needed"
 		exit 0
 	fi
 }
 
 TPM2Init () {
-	echo Defining the area on the TPM where we will store a $KEYSIZE character key...
+	echo "Defining the area on the TPM where we will store a $KEYSIZE character key..."
 	echo
 	tpm2_nvundefine $KEYADDRESS 2> /dev/null
 	tpm2_nvdefine -s $KEYSIZE $KEYADDRESS > /dev/null
 }
 
 TPM2Write () {
-	echo Storing the key in the TPM...
+	echo "Storing the key in the TPM..."
 	echo
 	tpm2_nvwrite -i $KEYFILE $KEYADDRESS
 }
 
 TPM2Verify () {
-	echo Checking the saved key against the one in the TPM...
+	echo "Checking the saved key against the one in the TPM..."
 	echo
 	tpm2_nvread -s $KEYSIZE $KEYADDRESS 2> /dev/null | diff $KEYFILE - > /dev/null
 	if [ $? != 0 ]
 	then
-		echo The $KEYFILE file does not match what is stored in the TPM.  Cannot proceed!
+		echo "The $KEYFILE file does not match what is stored in the TPM.  Cannot proceed!"
 		exit 1
 	fi
 }
@@ -149,38 +149,39 @@ TPM2Verify () {
 LuskDriveCheck () {
 	if $(cryptsetup isLuks $TARGET_DEVICE)
 	then
-		echo Using \"$TARGET_DEVICE\", which appears to be a valid LUKS encrypted device...
+		echo "Using \"$TARGET_DEVICE\", which appears to be a valid LUKS encrypted device..."
 	else
-		echo Device \"$TARGET_DEVICE\" does not appear to be a valid LUKS encrypted device.  Please specify a device on the command line, e.g.
-		echo sudo ./tpm2-luks-autounlock.sh /dev/sda3
+		echo "Device \"$TARGET_DEVICE\" does not appear to be a valid LUKS encrypted device.  Please specify a device on the command line, e.g."
+		echo "sudo ./tpm2-luks-autounlock.sh /dev/sda3"
 		exit 1
 	fi
 }
 
 LuksAddKey () {
-	echo Adding the new key to LUKS.  You will need to enter the current passphrase used to unlock the drive...
+	echo "Adding the new key to LUKS.  You will need to enter the current passphrase used to unlock the drive..."
 	echo
 	cryptsetup luksAddKey $TARGET_DEVICE $KEYFILE
 	if [ $? != 0 ]
 	then
-		echo Something went wrong adding the encryption key to $TARGET_DEVICE. Check /etc/crypttab and/or lsblk to determine your encrypted volume, then update this script with the correct value
+		echo "Something went wrong adding the encryption key to $TARGET_DEVICE."
+		echo "Check /etc/crypttab and/or lsblk to determine your encrypted volume, then update this script with the correct value"
 		exit 1
 	fi
 }
 
 LuksVerify () {
-	echo Checking the saved key against the one in the LUKS2...
+	echo "Checking the saved key against the one in the LUKS2..."
 	echo
 	cat $KEYFILE|cryptsetup open --test-passphrase $TARGET_DEVICE
 	if [ $? != 0 ]
 	then
-		echo The $KEYFILE file is not found in LUKS.  Cannot proceed!
+		echo "The $KEYFILE file is not found in LUKS. Cannot proceed!"
 		exit 1
 	fi
 }
 
 TPM2GetKeyInstall () {
-	echo Creating a key recovery script and putting it at /usr/local/sbin/tpm2-getkey...
+	echo "Creating a key recovery script and putting it at /usr/local/sbin/tpm2-getkey..."
 	echo
 	cat << EOF > /usr/local/sbin/tpm2-getkey
 #!/bin/sh
@@ -205,7 +206,7 @@ chmod 750 /usr/local/sbin/tpm2-getkey
 }
 
 TPM2DecryptKeyInstall () {
-	echo Creating initramfs hook and putting it at /etc/initramfs-tools/hooks/tpm2-decryptkey...
+	echo "Creating initramfs hook and putting it at /etc/initramfs-tools/hooks/tpm2-decryptkey..."
 	echo
 	cat << EOF > /etc/initramfs-tools/hooks/tpm2-decryptkey
 #!/bin/sh
@@ -257,34 +258,34 @@ Crypttab () {
 		exit 1
 	fi
 
-	echo Backing up /etc/crypttab to /etc/crypttab.bak, then updating it to run tpm2-getkey on decrypt...
+	echo "Backing up /etc/crypttab to /etc/crypttab.bak, then updating it to run tpm2-getkey on decrypt..."
 	echo
 	cp /etc/crypttab /etc/crypttab.bak
 	sed -i 's%$%,keyscript=/usr/local/sbin/tpm2-getkey%' /etc/crypttab
 
-	echo Copying the current initramfs just in case, then updating the initramfs with auto unlocking from the TPM...
+	echo "Copying the current initramfs just in case, then updating the initramfs with auto unlocking from the TPM..."
 	echo
 	cp /boot/initrd.img-$(uname -r) /boot/initrd.img-$(uname -r).orig
 	mkinitramfs -o /boot/initrd.img-$(uname -r) $(uname -r)
 }
 
 InfoMsg () {
-	echo At this point you are ready to reboot and try it out!
+	echo "At this point you are ready to reboot and try it out!"
 	echo
-	echo If the drive unlocks as expected, you may optionally remove the original password used to encrypt the drive and rely
-	echo completely on the random new one stored in the TPM.  If you do this, you should keep a copy of the key somewhere saved on
-	echo a DIFFERENT system, or printed and stored in a secure location on another system so you can manually enter it at the prompt.
-	echo To get a copy of your key for backup purposes, run this command:
-	echo sudo tpm2_nvread -s $KEYSIZE $KEYADDRESS
+	echo "If the drive unlocks as expected, you may optionally remove the original password used to encrypt the drive and rely"
+	echo "completely on the random new one stored in the TPM.  If you do this, you should keep a copy of the key somewhere saved on"
+	echo "a DIFFERENT system, or printed and stored in a secure location on another system so you can manually enter it at the prompt."
+	echo "To get a copy of your key for backup purposes, run this command:"
+	echo "sudo tpm2_nvread -s $KEYSIZE $KEYADDRESS"
 	echo
-	echo If you remove the original password used to encrypt the drive and fail to backup the key in then TPM then experience TPM,
-	echo motherboard, or another failure preventing auto-unlock, you WILL LOSE ACCESS TO EVERYTHING ON THE DRIVE!
-	echo If you are SURE you have a backup of the key you put in the TPM, here is the command to remove the original password:
-	echo sudo cryptsetup luksRemoveKey $TARGET_DEVICE
+	echo "If you remove the original password used to encrypt the drive and fail to backup the key in then TPM then experience TPM,"
+	echo "motherboard, or another failure preventing auto-unlock, you WILL LOSE ACCESS TO EVERYTHING ON THE DRIVE!"
+	echo "If you are SURE you have a backup of the key you put in the TPM, here is the command to remove the original password:"
+	echo "sudo cryptsetup luksRemoveKey $TARGET_DEVICE"
 	echo
-	echo If booting fails, press esc at the beginning of the boot to get to the grub menu.  Edit the Ubuntu entry and add .orig to end
-	echo of the initrd line to boot to the original initramfs this one time.
-	echo e.g. initrd /initrd.img-5.4.0-40-generic.orig
+	echo "If booting fails, press esc at the beginning of the boot to get to the grub menu.  Edit the Ubuntu entry and add .orig to end"
+	echo "of the initrd line to boot to the original initramfs this one time."
+	echo "e.g. initrd /initrd.img-5.4.0-40-generic.orig"
 }
 
 
